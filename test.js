88bdb42170f4c240e7fa021283502549b810abcc
plukenis <ieva.zaboole@gmail.com>
2021-08-11 15:56:16 +0300

    atempt

---
+++ test.js
@@ -0,0 +1,33 @@
+const data = [
+    {
+        name: 'Jonas',
+        marks: [10, 2, 8, 4, 6]
+    },
+    {
+        name: 'Maryte',
+        marks: [3, 7, 9, 5]
+    },
+    {
+        name: 'Petras',
+        marks: [7, 7, 7]
+    },
+    {
+        name: 'Ona',
+        marks: [10, 9, 8, 7, 6]
+    },
+]
+
+// PASTABA: jei kazkuri reikalavima atitinka daugiau nei vienas mokinys, spausdinti visus
+
+// Isspausdinti mokinio varda ir pazymi, kuris turi prasciausia pazymi
+
+    
+// Isspausdinti mokinio varda ir pazymi, kuris turi geriausia pazymi
+
+// Isspausdinti mokinio varda ir pazymiu vidurki, kuris turi prasciausia pazymiu vidurki
+
+// Isspausdinti mokinio varda ir pazymiu vidurki, kuris turi geriausia pazymiu vidurki
+
+// Isspausdinti mokinio varda, jei jis turi pazymi 8
+
+// Isspausdinti mokinio varda, jei jis neturi pazymio 7
